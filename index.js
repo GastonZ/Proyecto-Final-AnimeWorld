056ac204aafcf564546a09306e53f193aa2c49ad
GastonZ <gastonzappullaryu@gmail.com>
2022-09-05 17:37:27 -0300

    Commit de mejorando la main page

---
+++ index.js
@@ -0,0 +1,20 @@
+document.querySelector('.mirai').addEventListener("click", showTabs);
+
+const tabs = document.querySelector('.tab-container')
+const eachTab = document.querySelectorAll('.tab')
+
+function showTabs(e) {
+    e.target.removeEventListener(e.type, showTabs);
+    tabs.classList.toggle('tabs-show')
+    Toastify({
+
+        text: "W-Welcome, i'm Mirai , feel free to visit our different pages down here",
+        style: {
+            background: "linear-gradient(45deg, #292929, #1a2f3f)",
+          },
+        duration: 5000
+        
+        }).showToast();
+
+}
+
